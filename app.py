d28c34ba21bf0035250f006b30f141b2c287fe44
GU-CHE <127808636+GU-CHE@users.noreply.github.com>
2026-09-16 12:18:02 +0000

    1

--- app.py
+++ app.py
@@ -3,6 +3,10 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
+
 @app.route("/main",methods=["GET","POST"])
 def main():
     return(render_template("main.html"))
